8228c910522a692f5a2c68c4ffa9e3edf35b86b6
Francesco <francescogalimberti98@gmail.com>
2022-04-12 16:50:00 +0200

    a

--- settimo post.R
+++ settimo post.R
@@ -13,7 +13,7 @@ library(ggrepel)
 # showtext_auto()
   
 data <- read_excel("Dataset 7 post.xlsx")
-data$Zona = factor(data$Zona, levels = c('NordEst','NordOvest','Centro','Isole','Sud'), ordered = T)
+data$Zona = factor(data$Zona, levels = c('Nord Est','Nord Ovest','Centro','Isole','Sud'), ordered = T)
 
 ggplot(data, aes(y=panepasta, x = obesi, color = Zona)) +
   geom_point(size=3,show.legend = TRUE) +
@@ -32,12 +32,12 @@ ggplot(data, aes(y=panepasta, x = obesi, color = Zona)) +
 # PER LEGENDA
 
 ggplot(data, aes(y=panepasta, x = obesi, color = Zona)) +
-  geom_point(size=1,show.legend = T) +
-  geom_text_repel(aes(label = Regione),show.legend = T,family = 'Montserrat',fontface = "bold",size = 9.8) +
+  geom_point(size=15,show.legend = TRUE) +
+  geom_text_repel(aes(label = Regione),show.legend = FALSE,family = 'Montserrat',fontface = "bold",size = 2) +
   labs(y = "Pane e pasta", x = "% obesi",family = 'Montserrat',fontface = "bold") +
   scale_color_viridis(begin = 0.15,end = 0.65,direction = 1,discrete = TRUE, option = "F") +
-  theme_minimal(base_size=34, base_family = 'Montserrat') +
+  theme_minimal(base_size=25, base_family = 'Montserrat') +
   theme(axis.line = element_line(color='grey85'), panel.grid.minor = element_blank(),
         axis.title=element_text(face="bold"), legend.text = element_text(face="bold"),
-        axis.text = element_text(face="bold"))
+        axis.text = element_text(face="bold"),legend.direction="horizontal", legend.position = 'bottom')
 
