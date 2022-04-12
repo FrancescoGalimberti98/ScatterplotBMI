84d5808e5513d317e82b952837eaa9a40a054750
Francesco <francescogalimberti98@gmail.com>
2022-04-12 14:00:00 +0200

    a

--- settimo post.R
+++ settimo post.R
@@ -9,8 +9,11 @@ showtext_auto()
   
 data <- read_excel("Dataset 7 post.xlsx")
 
-ggplot(data, aes(y=panepasta, x=obesi, color=Regione))+
+ggplot(data, aes(y=panepasta, x = obesi, color = Zona)) +
   geom_point(size=10,show.legend = F) +
-  geom_text(aes(label = Regione),show.legend = F, nudge_y = 0.8,family = 'Montserrat',fontface = "bold") +
-  labs(y = "Panepasta", x = "Obesi",family = 'Montserrat',fontface = "bold") +
-  theme_minimal() 
+  geom_text(aes(label = Regione),show.legend = F, nudge_y = 0.8,family = 'Montserrat',fontface = "bold",size = 6) +
+  labs(y = "Pane e pasta", x = "% obesi",family = 'Montserrat',fontface = "bold") +
+  scale_fill_brewer(palette = 'Spectral', aesthetics = "colour", type = 'qual') +
+  theme_minimal(base_size=20, base_family = 'Montserrat.bold') 
+
+RColorBrewer::display.brewer.all()
